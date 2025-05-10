0fed0d1f3d683809bb585f07818a598abcc58c30
Dimitri <dimitri.timoz@orange.fr>
2025-05-10 17:19:50 +0300

    Fix status flag definitions and enhance CPU instruction handling in the step function

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -32,7 +32,8 @@ pub enum StatusFlags {
     Carry = 0b000_0001,
     Zero = 0b000_0010,
     InterruptDisable = 0b000_0100,
-    BFrlag = 0b000_1000,
+    Decimal = 0b000_1000,
+    BFlag = 0b001_0000,
     // Not used
     Overflow = 0b010_0000,
     Negative = 0b100_0000,
@@ -68,6 +69,17 @@ impl CPU {
         CPU::default()
     }
 
+    fn wait_n_cycle(&self, n: u8) {
+
+    }
+
+    fn get_next_byte(&mut self) -> u8 {
+        let byte = self.memory.read(self.pc);
+        self.pc += 1;
+        self.wait_n_cycle(1);
+        byte
+    }
+
     pub fn reset(&mut self) {
         self.pc = self.memory.read_reset_vector();
         self.s = 0xFD; 
@@ -79,10 +91,9 @@ impl CPU {
     }
 
     pub fn step(&mut self) {
-        let opcode = self.memory.read(self.pc);
+        let opcode = self.get_next_byte();
         let block = opcode & 0b11;
 
-        self.pc += 1;
         match block {
             0 => self.instruction_step(opcode),
             1 => self.alu_step(opcode),
@@ -93,12 +104,37 @@ impl CPU {
     }
 
     fn instruction_step(&mut self, opcode: u8) {
+        println!("INS OPCODE: {:#X}", opcode);
+
+        match opcode {
+            0x78 => { // SEI
+                self.p.set(StatusFlags::InterruptDisable);
+            }
+            0xD8 => { // CLD
+                self.p.clear(StatusFlags::BFlag);
+            }
+            _ => {
+                println!("Unknown instruction: {:#X}", opcode);
+            }
+        } 
     }
 
     fn alu_step(&mut self, opcode: u8) {
+        println!("ALU OPCODE: {:#X}", opcode);
+
     }
 
     fn rmw_step(&mut self, opcode: u8) {
+        println!("RMW OPCODE: {:#X}", opcode);
+        match opcode {
+            0xA2 => { // LDX #Immediate	
+                self.x = self.get_next_byte();
+            }
+            _ => {
+                println!("Unknown RMW instruction: {:#X}", opcode);
+            }
+            
+        }
     }
 
     fn unofficial_step(&mut self, opcode: u8) {
